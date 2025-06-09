cec5b2a484201a1624b8735fcc840bea50faf5bc
Deep Dhamala <dhamaladeep2@gmail.com>
2025-06-09 15:05:30 +0545

    initialize FilmPatro project with Maven and Spring Boot setup
    Signed-off-by: Deep Dhamala <dhamaladeep2@gmail.com>

---
+++ src/main/java/com/deepdhamala/filmpatro/FilmPatroApplication.java
@@ -0,0 +1,13 @@
+package com.deepdhamala.filmpatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmPatroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmPatroApplication.class, args);
+    }
+
+}
