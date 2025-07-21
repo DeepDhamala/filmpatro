6349a3249ce4cbdd0bb20ce28c2b3b4bbc368074
Deep Dhamala <dhamaladeep2@gmail.com>
2025-07-21 16:25:28 +0545

    Add Documentation
    Signed-off-by: Deep Dhamala <dhamaladeep2@gmail.com>

--- src/main/java/com/deepdhamala/filmpatro/auth/userRegistration/UserRegistrationService.java
+++ src/main/java/com/deepdhamala/filmpatro/auth/userRegistration/UserRegistrationService.java
@@ -1,7 +1,23 @@
 package com.deepdhamala.filmpatro.auth.userRegistration;
 
-
+/**
+ * Service interface for registering new users with different types of registration request dtos.
+ * @param <T> The type of registration request dto
+ */
 public interface UserRegistrationService <T>{
+
+    /**
+     * Registers a new user based on the provided registration request dto.
+     *
+     * @param requestDto The registration request dto containing user details.
+     */
     void registerNewUser(T requestDto);
+
+    /**
+     * Returns the class type of the supported registration request DTO.
+     * This is used to determine which implementation supports which DTO type.
+     *
+     * @return the Class object of the supported registration request DTO type
+     */
     Class<T> getSupportedDtoType();
 }
